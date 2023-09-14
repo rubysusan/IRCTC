0c40ac8d7082c5830f539971f0347a9c0dae29ab
AsnaPP <asna.pp@controlqore.com>
2023-09-14 17:21:12 +0530

    datetime diff

--- IRCTCapplicationAPI/IRCTCapplicationAPI/Request/Query/GetTrainByTrainTypeSearch/GetTrainByTrainTypeSearchQueryHandler.cs
+++ IRCTCapplicationAPI/IRCTCapplicationAPI/Request/Query/GetTrainByTrainTypeSearch/GetTrainByTrainTypeSearchQueryHandler.cs
@@ -15,7 +15,7 @@ namespace IRCTCapplicationAPI.Request.Query.GetTrainByTrainTypeSearch
         public async Task<List<ViewTrainByTrainTypeSearch>> Handle(GetTrainByTrainTypeSearchQuery request, CancellationToken cancellationToken)
         {
             return await _context.Train.Where(x => x.TrainStops.Select(x => x.Station.StationId).Contains(request.FromStationId) &&
-           x.TrainStops.Select(x => x.Station.StationId).Contains(request.ToStationId) && x.Date.Date == request.Date &&
+           x.TrainStops.Select(x => x.Station.StationId).Contains(request.ToStationId) && x.Date.Date == request.Date && EF.Functions.DateDiffHour(DateTime.Now, x.TrainStops.Where(y => y.Station.StationId == request.FromStationId).Select(z => z.ReachingTime).SingleOrDefault()) > 2 &&
              x.TrainClasses.Select(x => x.Coach.CoachId).Contains(request.CoachId) && x.TrainType.TrainTypeID == request.TypeId)
                 .Select(x => new ViewTrainByTrainTypeSearch
                 {
